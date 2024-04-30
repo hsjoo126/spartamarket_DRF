7b3814e06e7421543b7e6f1cc4351cfc8acac7c3
hsjoo126 <hsjoo126@naver.com>
2024-04-30 19:11:17 +0900

    products/ 상품조회기능 구현완료

--- spartamarket_DRF/products/views.py
+++ spartamarket_DRF/products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .models import Products
+from .serializers import ProductsSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class ProductListAPIView(APIView):
+    
+    def get(self, request):
+        products = Products.objects.all()
+        serializer = ProductsSerializer(products, many=True)
+        return Response(serializer.data)
