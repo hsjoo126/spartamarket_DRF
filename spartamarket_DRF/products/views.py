75bd34cdb06e3dc397dc266b9ece9bf89b705a11
hsjoo126 <hsjoo126@naver.com>
2024-04-30 23:07:53 +0900

    상품 등록 기능 구현 완료, 이미지 등록은 추후 추가예정, 로그인 상태에 따라서 get은 모든 사용자가, post는 로그인 사용자만 볼 수 있게 수정함 그래서 accounts에 토큰 관련 기능과 어드민 계정 추가했다, 기존에 products models.py 에 있던 모델도 author가 필요없을 거 같아서 삭제함.

--- spartamarket_DRF/products/views.py
+++ spartamarket_DRF/products/views.py
@@ -4,10 +4,22 @@ from rest_framework.views import APIView
 from .models import Products
 from .serializers import ProductsSerializer
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
 
 class ProductListAPIView(APIView):
+    
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
     def get(self, request):
         products = Products.objects.all()
         serializer = ProductsSerializer(products, many=True)
         return Response(serializer.data)
+    
+
+    def post(self, request):
+        serializer = ProductsSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        
