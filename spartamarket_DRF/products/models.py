49396c6a73609f39d2f6aa0d95781a741870c0bb
hsjoo126 <hsjoo126@naver.com>
2024-05-01 10:46:46 +0900

    상품 이미지 등록 기능 구현 완료, 모델에 image추가, settings에 설정들 추가, static폴더 추가, 주의사항:postman에서 실행시 raw가 아닌 form-data로 보내야함

--- spartamarket_DRF/products/models.py
+++ spartamarket_DRF/products/models.py
@@ -5,3 +5,4 @@ from django.conf import settings
 class Products(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    image = models.ImageField(upload_to="images/",blank=True)
